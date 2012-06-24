7f6ace05b6bd729c5e1c823fc4c896a0f634da83
metameta <metameta.git@gmail.com>
2012-06-24 23:45:59 +0900

    require 削除

--- test.meta.js
+++ test.meta.js
@@ -4,6 +4,5 @@
 // @version 1.0.1.12
 // @namespace test
 // @include http://*.sengokuixa.jp/*
-// @require http://code.jquery.com/jquery-1.7.2.min.js
 // @updateURL https://raw.github.com/metameta/test/master/test.meta.js
 // ==/UserScript==
