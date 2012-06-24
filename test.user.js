04bb1208da2ce317768a9dafb9515ee91ed7e42f
metameta <metameta.git@gmail.com>
2012-06-24 21:48:00 +0900

    test

---
+++ test.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name test
+// @description test
+// @version 1.0.1.12
+// @namespace test
+// @include http://*.sengokuixa.jp/*
+// @require http://code.jquery.com/jquery-1.7.2.min.js
+// @updateURL https://raw.github.com/metameta/test/master/test.meta.js
+// ==/UserScript==
