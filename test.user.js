e02d76d3f4643e8f7ebdbb48b007ca25f59c2f5f
metameta <metameta.git@gmail.com>
2012-07-15 20:59:10 +0900

    iconテスト

--- test.user.js
+++ test.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name test
 // @description test
-// @version 1.0.1.12
+// @version 1.0.1.11
 // @namespace test
 // @include http://*.sengokuixa.jp/*
 // @updateURL https://raw.github.com/metameta/test/master/test.meta.js
+// @iconURL https://github.com/downloads/metameta/test/icon_64.png
 // ==/UserScript==
