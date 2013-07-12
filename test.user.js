7ece654a702c5764c854ed4ba905e2d253f7ca37
metameta <metameta.git@gmail.com>
2013-07-12 22:28:41 +0900

    #1

--- test.user.js
+++ test.user.js
@@ -5,5 +5,4 @@
 // @namespace test
 // @include http://*.sengokuixa.jp/*
 // @updateURL https://raw.github.com/metameta/test/master/test.meta.js
-// @iconURL https://github.com/downloads/metameta/test/icon_64.png
 // ==/UserScript==
