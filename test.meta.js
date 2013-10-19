e0ba7731cc0b91eddbb044faa74cf251f1763d9f
metameta <metameta.git@gmail.com>
2013-10-19 21:55:14 +0900

    beta

--- test.meta.js
+++ test.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name test
 // @description test
-// @version 1.0.1.12
+// @version 1.0.1.13bata1
 // @namespace test
 // @include http://*.sengokuixa.jp/*
 // @updateURL https://raw.github.com/metameta/test/master/test.meta.js
