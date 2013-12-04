793a0627f5efa1f978b5c37213ec6ae8bd651e93
metameta <metameta.git@gmail.com>
2013-12-04 21:47:42 +0900

    コミットメッセージ
    Issue #5

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name test
 // @description test
-// @version 1.0.1.13
+// @version 1.0.1.14
 // @namespace test
 // @include http://*.sengokuixa.jp/*
 // @updateURL https://raw.github.com/metameta/test/master/test.meta.js
